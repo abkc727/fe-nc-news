4ee3eaf5839784e3cda5e9e9edfc0085454e85e9
abkc727 <adityababu225@gmail.com>
2023-12-12 15:00:45 +0000

    removed comments from the code

--- src/components/ArticlesContainer.jsx
+++ src/components/ArticlesContainer.jsx
@@ -1,55 +1,30 @@
-// import { useNavigate  } from 'react-router-dom';
-
-import { useEffect, useState } from "react"
-import { ArticleCard } from "./ArticleCard"
-import { getArticles } from "../utils/api"
-
+import { useEffect, useState } from "react";
+import { ArticleCard } from "./ArticleCard";
+import { getArticles } from "../utils/api";
 
 export const ArticlesContainer = () => {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [articles, setArticles] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  // const navigate = useNavigate();
+  useEffect(() => {
+    getArticles().then((articlesData) => {
+      setArticles(articlesData);
+      setIsLoading(false);
+    });
+  }, []);
 
-  useEffect(()=> {
-
-    getArticles().then((articlesData)=> {
-        setArticles(articlesData)
-        setIsLoading(false)
-    })
-}, [])
-
-// useEffect(() => {
-
-//   if (selectedArticleId !== null) {
-//     navigate(`/article/${selectedArticleId}`);
-//   }
-// }, [selectedArticleId, navigate]);
-
-
-  if(isLoading) {
-
-    return <p>Loading...</p>
-}
-
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
-
     <div className="articlesContainer">
       <h3>Articles</h3>
       <ul className="article_box">
-                {articles.map((article)=> {
-                   
-                    return <ArticleCard key = {article.article_id} article = {article}   />
-
-                })}
-            </ul>
-            
-      
+        {articles.map((article) => {
+          return <ArticleCard key={article.article_id} article={article} />;
+        })}
+      </ul>
     </div>
-
-
-  )
-    
-   }
-
+  );
+};
